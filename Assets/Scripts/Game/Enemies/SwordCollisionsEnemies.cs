ec4f18733b6a80a74575203d1b47e5064ac8d0f4
Jorge Salmon <kokesalmon@gmail.com>
2022-04-29 19:32:23 +0200

    [FEATURE] Añadidas vidas, muertes al chocar espada y pantalla de game over.

---
+++ Assets/Scripts/Game/Enemies/SwordCollisionsEnemies.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordCollisionsEnemies : MonoBehaviour
+{
+    private AudioSource sonido;
+    public AudioClip clashSword;
+    public AudioClip killSword;
+    public float vol = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         sonido = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    private void OnCollisionEnter(Collision c){
+        GameObject ob = GameObject.Find("Sword");
+        if(c.collider.name.Equals(ob.name)){
+            if(clashSword != null){
+                sonido.PlayOneShot(clashSword, vol);
+            }
+            new WaitForSeconds(0.9f);
+        }else{
+            sonido.PlayOneShot(killSword, vol);
+            if(c.collider.name.Equals("MainCharacter")){
+                killing();
+            }
+            new WaitForSeconds(0.9f);
+        }
+    }
+        public void killing(){
+        if(GameObject.Find("Live1")){
+            Destroy(GameObject.Find("Live1"));
+        }else if(GameObject.Find("Live2")){
+            Destroy(GameObject.Find("Live2"));
+        }else if(GameObject.Find("Live3")){
+            Destroy(GameObject.Find("Live3"));
+            GameObject ob1 = GameObject.Find("MainCharacter");
+            Animator anim = ob1.GetComponent<Animator>();
+            anim.SetBool("Hit",true);
+        }
+    }
+
+}
